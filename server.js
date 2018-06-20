31eab7a194bf580c4c4adfc04dac85a9a09ef113
a.beinarovics <abeinarovics@evolutiongaming.com>
2018-06-20 13:46:37 +0300

    post points

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var port = process.env.PORT || 3000;
 
-const points = {
+let points = {
     p1:{
         id: "C3",
         signal: -72
@@ -17,8 +17,13 @@ const points = {
 }
 
 var app = express();
+app.use(express.bodyParser());
 app.get("/", function (req, res) {
     res.send(JSON.stringify(points));
+});
+
+app.post("/points", function (req) {
+    points.push(req.body)
 });
 app.listen(port, function () {
     console.log(`Example app listening on port !`);
